9525e26b5f8c8522a79471d1741753a83ef9fa8f
Meng Zhang <meng@Mengs-MacBook-Pro.local>
2019-02-20 15:30:40 +1300

    Example Tic-Tac-Toe

---
+++ SS Examples/C10-Tic-Tac-Toe.scm
@@ -0,0 +1,143 @@
+; C10 Tic-Tac-Toe
+
+
+; ***************************** Find-triples: finds all the letters in all eight winning combinations.
+
+; ************* Substitute-triple: finds all three letters corresponding to three squares.
+
+; ******* Substitute-letter: finds the letter in a single square.
+(define (substitute-letter square position)
+  (if (equal? '_ (item square position))
+      square
+      (item square position)))
+
+(define (substitute-triple combination position)
+  (accumulate word
+              (every (lambda (square)
+                       (substitute-letter square position))
+                     combination)))
+
+(define (find-triples position)
+  (every (lambda (comb) (substitute-triple comb position))
+         '(123 456 789 147 258 369 159 357)))
+
+; ***************************** I-can-win?
+
+; ************* choose-win
+
+; ******* My-pair
+
+; **** Opponent
+(define (opponent letter)
+  (if (equal? letter 'x) 'o 'x))
+
+(define (my-pair? triple me)
+  (and (= (appearances me triple) 2)
+       (= (appearances (opponent me) triple) 0)))
+
+(define (choose-win winning-triples)
+  (if (empty? winning-triples)
+      #f
+      (keep number? (first winning-triples))))
+
+(define (i-can-win? triples me)
+  (choose-win
+    (keep (lambda (triple) (my-pair? triple me))
+          triples)))
+
+; ***************************** Opponent-can-win?
+(define (opponent-can-win? triples me)
+  (i-can-win? triples (opponent me)))
+
+; ***************************** I-can-fork?
+
+; ************* First-if-any
+
+; ******* Pivots
+
+; **** My-single?
+
+; ** Reapeated-numbers
+
+; * Sort-digits
+
+; Extract-digit?
+(define (extract-digit desired-digit wd)
+  (keep (lambda (wd-digit) (equal? wd-digit desired-digit))
+        wd))
+
+(define (sort-digits number-word)
+  (every (lambda (digit) (extract-digit digit number-word))
+         '(1 2 3 4 5 6 7 8 9)))
+
+(define (repeated-numbers sent)
+  (every first
+         (keep (lambda (wd) (>= (count wd) 2))
+               (sort-digits (accumulate word sent)))))
+
+(define (my-single? triple me)
+  (and (= (appearances me triple) 1)
+       (= (appearances (opponet me) triple) 0)))
+
+(define (pivots triples me)
+  (repeated-numbers (keep (lambda (triple) (my-single? triple me))
+                          triples)))
+
+(define (first-if-any sent)
+  (if (empty? sent)
+      #f
+      (first sent)))
+
+(define (i-can-fork? triples me)
+  (first-if-any (pivots triples me)))
+
+; ***************************** I-can-advance?
+
+; ************* Best-move
+
+; ******* Best-square
+
+; **** Best-square-helper
+(define (best-square-helper opponent-pivots pair)
+  (if (member? (first pair) opponent-pivots)
+      (first pair)
+      (last pair)))
+
+(define (best-square my-triple triples me)
+  (best-square-helper (pivots triples (opponent me))
+                      (keep number? my-triple)))
+
+(define (best-move my-triples all-triples me)
+  (if (empty? my-triples)
+      #f
+      (best-square (first my-triples) all-triples me)))
+
+(define (i-can-advance? triples me)
+  (best-move (keep (lambda (triple) (my-single? triple me)) triples)
+             triples
+             me))
+
+; ***************************** Best-free-square
+
+; ************* First-choice
+(define (first-choice possibilities preferences)
+  (first (keep (lambda (square) (member? square possibilities))
+               preferences)))
+
+(define (best-free-square triples)
+  (first-choice (accumulate word triples)
+                ’(5 1 3 7 9 2 4 6 8)))
+
+; ***************************** ttt-choose
+(define (ttt-choose triples me)
+  (cond ((i-can-win? triples me))
+        ((opponent-can-win? triples me))
+        ((i-can-fork? triples me))
+        ((i-can-advance? triples me))
+        (else (best-free-square triples))))
+
+; ***************************** ttt
+(define (ttt position me)
+  (ttt-choose (find-triples position) me))
+
+
---
+++ SS Exercises/Exercises 10.1-10.4.scm
@@ -0,0 +1,78 @@
+; Exercises 10.1-10.4
+
+; 10.1 The ttt procedure assumes that nobody has won the game yet. What happens if you invoke ttt with a board position in which some player has already won? Try to figure it out by looking through the program before you run it.
+
+; answer: the ttt procedure would continue going through the existing conditions and excucate the corresponding results, and return an error messsage -no argument for `first`- unitl all the sqaures are occupied by the players.
+
+; A complete tic-tac-toe program would need to stop when one of the two players wins. Write a predicate already-won? that takes a board position and a letter (x or o) as its arguments and returns #t if that player has already won.
+
+; answer:
+(define (already-won? position me)
+  (member? (word me me me) (find-triples position)))
+; **********************************************************
+
+; 10.2 The program also doesn’t notice when the game has ended in a tie, that is, when all nine squares are already filled. What happens now if you ask it to move in this situation?
+
+; answer:the ttt procedure would return an error messsage -no argument for `first`.
+
+;Write a procedure tie-game? that returns #t in this case.
+
+; answer:
+(define (tie-game-full? position)
+  (not (member? '_ position)))
+; **********************************************************
+
+; 10.3 A real human playing tic-tac-toe would look at a board like this:
+
+; [image]('oxooxxxo9)
+
+;and notice that it’s a tie, rather than moving in square 9. Modify tie-game? from Exercise 10.2 to notice this situation and return #t.
+
+;(Can you improve the program’s ability to recognize ties even further? What about boards with two free squares?)
+
+; answer:
+(define (tie-game-oneFreeSquare? position me)
+  (and (= (appearances '_ position) 1)
+       (not (i-can-win? (find-triples position) me))))
+
+(define (tie-game-twoFreeSquares? position me)
+  (and (= (appearances '_ position) 2)
+       (not (i-can-win? (find-triples position) me))
+       (not (i-can-win? (find-triples position) (opponent me)))))
+
+
+
+(define (tie-game? position me)
+  (cond ((= (appearances '_ position) 2)
+         (and (not (i-can-win? (find-triples position) me))
+              (not (i-can-win? (find-triples position) (opponent me)))))
+        ((= (appearances '_ position) 1)
+         (not (i-can-win? (find-triples position) me)))
+        (else #f)))
+
+;; test cases for the tied games
+(tie-game? 'oxoox_xo_ 'x)  ;t  ;; test if two empty spots will lead to a tie
+(tie-game? 'oxooxoxo_ 'x)  ;t  ;; test if a tie if there are two 'o shapes on a triple and one space
+(tie-game? 'oxxoxo_o_ 'x)  ;f  ;; test if a tie if there are two 'o and two 'x shapes on two triple and two spaces
+(tie-game? 'ox_xox_ox 'o)  ;f  ;; test if a tie if there are two 'x shapes on a triple and two spaces
+
+; **********************************************************
+
+; 10.4 Here are some possible changes to the rules of tic-tac-toe:
+; • What if you could win a game by having three squares forming an L shape in a corner, such as squares 1, 2, and 4?
+; • What if the diagonals didn’t win?
+; • What if you could win by having four squares in a corner, such as 1, 2, 4, and 5?
+
+; Answer the following questions for each of these modifications separately: What would happen if we tried to implement the change merely by changing the quoted sentence of potential winning combinations in find-triples? Would the program successfully follow the rules as modified?
+
+; answer:
+; Changing the quoted sentence of potential winning combinations for L shape winning rule would not interrupt the program.
+; Changing the quoted sentence of potential winning combinations for no diagonals winning rule would not interrupt the program.
+; Changing the quoted sentence of potential winning combinations for square winning rule would interrupt the program, because `i-can-win?`, `opponent-can-win?` `i-can-fork?` and `i-can-advance?` couldn't work on four-element triples.
+
+; **********************************************************
+
+; 10.5 Modify ttt to play chess.
+
+
+
