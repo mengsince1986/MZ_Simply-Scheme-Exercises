6f9541c9cb2e8bfd5b3c9c935926747a571d1e6d
Meng Zhang <meng@Mengs-MacBook-Pro.local>
2019-02-22 12:32:10 +1300

    C11-explode-letter-pairs

---
+++ SS Exercises/C11-explode-letter-pairs.scm
@@ -0,0 +1,26 @@
+; **********************************************************
+
+(explode 'dynamite)
+; (D Y N A M I T E)
+
+; **********************************************************
+
+(define (explode wd)
+  (if (<= (count wd) 1)
+      (sentence wd)
+      (sentence (first wd)
+                (explode (butfirst wd)))))
+
+
+; **********************************************************
+
+(letter-pairs 'george)
+; (GE EO OR RG GE)
+
+; **********************************************************
+
+(define (letter-pairs wd)
+  (if (<= (count wd) 2)
+      (sentence wd)
+      (sentence (letter-pairs (butlast wd))
+                (word (last (butlast wd)) (last wd)))))
