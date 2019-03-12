3063c7e8d2fcd0c40895abe86e350001d40ed356
Meng Zhang <meng@Mengs-MacBook-Pro.local>
2019-03-12 15:17:33 +1300

    Exercises 14.1-14.16

--- SS Exercises/Exercises 8.4-8.14.scm
+++ SS Exercises/Exercises 8.4-8.14.scm
@@ -81,7 +81,7 @@
 ; **********************************************************
 
 ; 8.7 [14.5] Write a procedure letter-count that takes a sentence as its argument and returns the total number of letters in the sentence:
-(letter-count ’(fixing a hole)) ; 11
+(letter-count '(fixing a hole)) ; 11
 
 ;answer:
 (define (letter-count sentence)
