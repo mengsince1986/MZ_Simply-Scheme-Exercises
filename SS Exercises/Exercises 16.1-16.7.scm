d0a1c77bf733c901520ea5e6eefd7f71a794c579
Meng Zhang <meng@Mengs-MacBook-Pro.local>
2019-04-15 23:30:51 +1200

    Exercises 16.1-16.7 in progress

---
+++ SS Exercises/Exercises 16.1-16.7.scm
@@ -0,0 +1,13 @@
+; Exercises 16.1-16.7
+
+; 16.1 Design and test a pattern that matches any sentence containing the word C three times (not necessarily next to each other).
+
+; solution:
+'(* C * C * C *)
+
+; **********************************************************
+
+; 16.2 Design and test a pattern that matches a sentence consisting of two copies of a smaller sentence, such as (a b a b).
+
+; solution:
+'(*sent *sent)
