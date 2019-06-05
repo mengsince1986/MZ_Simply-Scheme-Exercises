285bbe0ec9b03ecbaf391603087b506518e1b634
mz <mengcode1986@gmail.com>
2019-06-05 16:36:21 +1200

    Exercises 8.4-8.14 refactored

--- SS Exercises/Exercises 8.4-8.14.scm
+++ SS Exercises/Exercises 8.4-8.14.scm
@@ -132,10 +132,10 @@
 
 ; **********************************************************
 
-; 8.10 Write a predicate true-for-all? that takes two arguments, a predicate proce- dure and a sentence. It should return #t if the predicate argument returns true for ever y word in the sentence.
-(true-for-all? even? ’(2 4 6 8))
+; 8.10 Write a predicate true-for-all? that takes two arguments, a predicate procedure and a sentence. It should return #t if the predicate argument returns true for every word in the sentence.
+(true-for-all? even? '(2 4 6 8))
 ; #T
-(true-for-all? even? ’(2 6 3 4))
+(true-for-all? even? '(2 6 3 4))
 ; #F
 
 ; anwser:
