3bd434b17d4f8aebd4f248287baf219ee26ee332
mz <mengcode1986@gmail.com>
2019-06-05 16:36:52 +1200

    Exercise 19.10

--- SS Exercises/Exercises 19.2-19.13.scm
+++ SS Exercises/Exercises 19.2-19.13.scm
@@ -77,3 +77,172 @@
 ; #F
 ; ----------
 
+(define (true-for-all? pred sent)
+  (if (null? sent)
+      #f
+      (true-for-all-helper pred sent)))
+
+(define (true-for-all-helper pred sent)
+  (cond ((null? sent) #t)
+        ((not (pred (car sent))) #f)
+        (else (true-for-all-helper pred (cdr sent)))))
+
+; **********************************************************
+
+; 19.6 Write a procedure true-for-any-pair? that takes a predicate and a sentence as arguments. The predicate must accept two words as its arguments. Your procedure should return #t if the argument predicate will return true for any two adjacent words in the sentence:
+
+(true-for-any-pair? equal? '(a b c b a))
+; #F
+
+(true-for-any-pair? equal? '(a b c c d))
+; #T
+
+(true-for-any-pair? < '(20 16 5 8 6))
+; #T
+
+; solution:
+
+(define (true-for-any-pair? pred sent)
+  (cond ((null? (cdr sent)) #f)
+        ((pred (car sent) (cadr sent)) #t)
+        (else (true-for-any-pair? pred (cdr sent)))))
+
+; **********************************************************
+
+; 19.7 Write a procedure true-for-all-pairs? that takes a predicate and a sentence as arguments. The predicate must accept two words as its arguments. Your procedure should return #t if the argument predicate will return true for every two adjacent words in the sentence:
+
+(true-for-all-pairs? equal? '(a b c c d))
+; #F
+
+(true-for-all-pairs? equal? '(a a a a a))
+; #T
+
+(true-for-all-pairs? < '(20 16 5 8 6))
+; #F
+
+(true-for-all-pairs? < '(3 7 19 22 43))
+; #T
+
+; solution:
+
+(define (true-for-all-pairs? pred sent)
+  (let ((1st-equal-2nd? (pred (car sent) (cadr sent))))
+    (cond ((= (length sent) 2) 1st-equal-2nd?)
+          ((not 1st-equal-2nd?) #f)
+          (else (true-for-all-pairs? pred (cdr sent))))))
+
+; **********************************************************
+
+; 19.8 Rewrite true-for-all-pairs? (Exercise 19.7) using true-for-any-pair?  (Exercise 19.6) as a helper procedure. Don’t use recursion in solving this problem (except for the recursion you’ve already used to write true-for-any-pair? ). Hint: You’ll find the not procedure helpful.
+
+; solution:
+
+(define (true-for-all-pairs? pred sent)
+  (not (true-for-any-pair? (lambda (a b) (not (pred a b)))
+                           sent)))
+
+; **********************************************************
+
+; 19.9 Rewrite either of the sort procedures from Chapter 15 to take two arguments, a list and a predicate. It should sort the elements of that list according to the given predicate:
+
+; ----------
+; the selection sort in Chapter 15
+
+(define (sort sent)
+  (if (empty? sent)
+      '()
+      (se (earliest-word sent)
+          (sort (remove-once (earliest-word sent) sent)))))
+
+(define (earliest-word sent)
+  (earliest-helper (first sent) (bf sent)))
+
+(define (earliest-helper so-far rest)
+  (cond ((empty? rest) so-far)
+        ((before? so-far (first rest))
+         (earliest-helper so-far (bf rest)))
+        (else (earliest-helper (first rest) (bf rest)))))
+
+(define (remove-once wd sent)
+  (cond ((empty? sent) '())
+        ((equal? wd (first sent)) (bf sent))
+        (else (se (first sent) (remove-once wd (bf sent))))))
+; ----------
+
+(sort '(4 23 7 5 16 3) <)
+; (3 4 5 7 16 23)
+
+(sort '(4 23 7 5 16 3) >)
+; (23 16 7 5 4 3)
+
+(sort '(john paul george ringo) before?)
+; (GEORGE JOHN PAUL RINGO)
+
+; solution:
+
+(define (sort lst pred)
+  (if (null? lst)
+      '()
+      (cons (head-element lst pred)
+            (sort (remove-once (head-element lst pred) lst) pred))))
+
+(define (head-element lst pred)
+  (head-element-helper (car lst) (cdr lst) pred))
+
+(define (head-element-helper so-far rest pred)
+  (cond ((null? rest) so-far)
+        ((pred so-far (car rest))
+         (head-element-helper so-far (cdr rest) pred))
+        (else (head-element-helper (car rest) (cdr rest) pred))))
+
+(define (remove-once element lst)
+  (cond ((null? lst) '())
+        ((equal? element (car lst)) (cdr lst))
+        (else (cons (car lst) (remove-once element (cdr lst))))))
+
+; **********************************************************
+
+; 19.10 Write tree-map, analogous to our deep-map, but for trees, using the datum and children selectors.
+
+; -------------------- tree constructor/selectors/predicates
+(define (make-node datum children)
+  (cons datum children))
+
+(define (leaf datum)
+  (make-node datum '()))
+
+(define (leaf? node)
+  (null? (children node)))
+
+(define (datum node)
+  (car node))
+
+(define (children node)
+  (cdr node))
+; --------------------
+
+; solution:
+
+(define (tree-map fn tree)
+  (if (leaf? tree)
+      (leaf (fn (datum tree)))
+      (cons (fn (datum tree))
+            (forest-map fn (children tree)))))
+
+(define (forest-map fn forest)
+  (if (null? forest)
+      '()
+      (cons (tree-map fn (car forest))
+            (forest-map fn (cdr forest)))))
+
+; **********************************************************
+
+
+
+
+
+
+
+
+
+
