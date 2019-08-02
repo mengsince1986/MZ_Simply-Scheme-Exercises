be4236614960bb57f568de18b9ebbe7d2b737929
mz <mengcode1986@gmail.com>
2019-08-02 22:21:15 +1200

    exercise 23.3

--- SS Exercises/Exercises 23.1-23.16.scm
+++ SS Exercises/Exercises 23.1-23.16.scm
@@ -36,3 +36,45 @@ vec
 
 ; solution:
 
+(define (vector-fill! vec ele)
+  (vc-fill!-helper vec ele (- (vector-length vec) 1)))
+
+(define (vc-fill!-helper vec ele index)
+  (if (< index 0)
+      vec
+      (begin (vector-set! vec index ele)
+             (vc-fill!-helper vec ele (- index 1)))))
+
+; **********************************************************
+
+; 23.3 Write a function vector-append that works just like regular append , but for vectors:
+
+(vector-append '#(not a) '#(second time))
+; #(not a second time)
+
+(define (vector-append vec-a vec-b)
+  (let ((index-a (- (vector-length vec-a) 1))
+        (index-b (- (vector-length vec-b) 1))
+        (vec-ab (make-vector (+ (vector-length vec-a)
+                                (vector-length vec-b)))))
+    (vec-append-helper vec-ab (- (vector-length vec-ab) 1)
+                       vec-a index-a
+                       vec-b index-b)))
+
+(define (vec-append-helper vec-ab index-ab
+                           vec-a index-a
+                           vec-b index-b)
+  (copy-vec vec-ab index-ab vec-b index-b)
+  (copy-vec vec-ab (- index-ab (vector-length vec-b))
+            vec-a index-a)
+  vec-ab)
+
+(define (copy-vec vec-out start vec-in index-in)
+  (if (< index-in 0)
+      'done
+      (begin (vector-set! vec-out start
+                          (vector-ref vec-in index-in))
+             (copy-vec vec-out (- start 1)
+                       vec-in (- index-in 1)))))
+
+
